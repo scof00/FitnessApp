97d0bc0a6a3520a38106c6ec54af6202ddea3aef
scof00 <scofprofessional@gmail.com>
2024-10-06 16:56:32 -0400

    Completed exercise edit

--- client/src/Components/Exercises/ExerciseEdit.jsx
+++ client/src/Components/Exercises/ExerciseEdit.jsx
@@ -1,5 +1,89 @@
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { getMuscles } from "../../Managers/MuscleManager";
+import { Input } from "reactstrap";
+import { useEffect, useState } from "react";
+import { GetExerciseById, updateExercise } from "../../Managers/ExerciseManager";
+
 export const ExerciseEdit = () => {
-    return(
-        <div>exercise edit</div>
-    )
+    const [exercise, setExercise] = useState({});
+    const [muscles, setMuscles] = useState([]);
+    const {exerciseId} =useParams();
+  
+    const navigate = useNavigate();
+    useEffect(() => {
+      getMuscles().then((data) => setMuscles(data));
+    }, []);
+    useEffect(() =>{
+        GetExerciseById(exerciseId).then((data) => {
+            setExercise(data)
+        })
+    },[])
+  
+    const handleSave = (event) => {
+      event.preventDefault();
+      if (!exercise.muscleId) {
+        window.alert("Please select muscle group!");
+      } else if (!exercise.name) {
+        window.alert("Please enter an exercise name!");
+      } else {
+        const newExercise = {
+          name: exercise.name,
+          muscleId: exercise.muscleId,
+          id: exercise.id,
+          userId: exercise.userId
+        };
+        updateExercise(newExercise).then(() => {
+          navigate("/exercises");
+        });
+      }
+    };
+  
+    return (
+      <form className="exerciseCreateForm">
+        <h2>Add a new exercise</h2>
+        <fieldset>
+          <Input
+          className="exerciseFormInput"
+            id="muscleSelect"
+            name="select"
+            type="select"
+            required
+            onChange={(event) => {
+              const exerciseCopy = { ...exercise };
+              exerciseCopy.muscleId = parseInt(event.target.value);
+              setExercise(exerciseCopy);
+            }}
+          >
+            <option value={0}>Select Muscle</option>
+            {muscles.map((m) => {
+              return (
+                <option key={m.id} value={m.id}>
+                  {m.name}
+                </option>
+              );
+            })}
+          </Input>
+        </fieldset>
+        <fieldset>
+          <Input
+            id="exerciseName"
+            name="exerciseName"
+            placeholder={exercise.name}
+            className="exerciseFormInput"
+            required
+            onChange={(event) => {
+              const exerciseCopy = { ...exercise };
+              exerciseCopy.name = event.target.value;
+              setExercise(exerciseCopy);
+            }}
+          ></Input>
+        </fieldset>
+        <fieldset>
+          <button onClick={handleSave} className="exerciseButton">Save</button>
+        </fieldset>
+        <Link to="/exercises">
+        <p>Cancel</p>
+        </Link>
+      </form>
+    );
 }
--- client/src/Managers/ExerciseManager.jsx
+++ client/src/Managers/ExerciseManager.jsx
@@ -11,3 +11,17 @@ export const createExercise = (e) => {
         body:JSON.stringify(e)
     })
 }
+
+export const GetExerciseById = (id) => {
+    return fetch(`https://localhost:5001/api/Exercise/${id}`).then((res) => res.json())
+}
+
+export const updateExercise = (e) => {
+    return fetch(`https://localhost:5001/api/Exercise/${e.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(e)
+    })
+}
