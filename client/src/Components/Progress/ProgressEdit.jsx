450044aade36694b3e61bd25c73979c36bc015f0
scof00 <scofprofessional@gmail.com>
2024-10-09 14:00:42 -0400

    change

--- client/src/Components/Progress/ProgressEdit.jsx
+++ client/src/Components/Progress/ProgressEdit.jsx
@@ -41,91 +41,6 @@ export const ProgressEdit = () => {
 
   return (
     <div className="progressList">
-      <div className="backButton">
-        <ArrowLeftSquare
-          size={30}
-          onClick={(event) => {
-            navigate(`/progress/details/${progress.exerciseId}`);
-          }}
-        />
-      </div>
-      <div>
-        Edit progress entry for: <b>{progress?.exercise?.name}</b> on{" "}
-        {progress.dateCompleted}
-      </div>
-      <div className="progressInfo">
-        <Label>Sets:</Label>
-        <Input
-          className="progressInput"
-          required
-          type="number"
-          placeholder={progress.sets}
-          onChange={(event) => {
-            const progressCopy = { ...progress };
-            progressCopy.sets = parseInt(event.target.value);
-            setProgress(progressCopy);
-          }}
-        ></Input>
-        <Label>Reps</Label>
-        <Input
-          required
-          className="progressInput"
-          type="number"
-          placeholder={progress.reps}
-          onChange={(event) => {
-            const progressCopy = { ...progress };
-            progressCopy.reps = parseInt(event.target.value);
-            setProgress(progressCopy);
-          }}
-        ></Input>
-      </div>
-      <div className="progressInfo">
-        <Label>Weight</Label>
-        <Input
-          className="progressInput"
-          required
-          type="number"
-          placeholder={progress.weight}
-          onChange={(event) => {
-            const progressCopy = { ...progress };
-            progressCopy.weight = parseInt(event.target.value);
-            setProgress(progressCopy);
-          }}
-        ></Input>
-        <Input
-          type="select"
-          required
-          className="progressInput"
-          onChange={(event) => {
-            const progressCopy = { ...progress };
-            progressCopy.weightType = event.target.value;
-            setProgress(progressCopy);
-          }}
-        >
-          <option>Weight</option>
-          <option value="lbs">lbs</option>
-          <option value="kgs">kgs</option>
-        </Input>
-      </div>
-      <Label>Notes:</Label>
-      <Input
-        className="notesInput"
-        placeholder={progress.notes}
-        onChange={(event) => {
-          const progressCopy = { ...progress };
-          progressCopy.notes = event.target.value;
-          setProgress(progressCopy);
-        }}
-      ></Input>
-      <Input
-        type="date"
-        onChange={(event) => {
-          const progressCopy = { ...progress };
-          progressCopy.dateCompleted = event.target.value;
-          setProgress(progressCopy);
-        }}
-      ></Input>
-    <div className="progressList">
       <div>
         <div className="backButton">
           <ArrowLeft
@@ -215,6 +130,7 @@ export const ProgressEdit = () => {
       <button className="exerciseButton" onClick={handleSubmit}>
         Save
       </button>
+    </div>
     </div>
   );
 };
